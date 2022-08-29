fff0e2bd636c22fc66f92ad5af29742323449840
Sudhir <sudirsha02@gmail.com>
2022-08-29 19:52:05 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+// Importing express module
 const express = require('express');
-const dotenv  = require('dotenv').config()
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
 const app = express();
+
+// Getting Request
 app.get('/', (req, res) => {
-  res.send('Hello World'+process.env.MESSAGE);
-});
 
-app.listen(PORT, ()=>{
-console.log(`Running on http://${HOST}:${PORT}`);
+	// Sending the response
+	res.send('Hello World!'+process.env.MESSAGE)
+	
+	// Ending the response
+	res.end()
+})
 
+// Establishing the port
+const PORT = process.env.PORT ||5000;
 
-});
+// Executing the server on given port number
+app.listen(PORT, console.log(
+`Server started on port ${PORT}`));
